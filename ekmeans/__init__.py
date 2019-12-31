4bcc0eebb428520bd37b73bbf13da4bd6a6afb6e
lovit <soy.lovit@gmail.com>
2019-12-31 12:09:12 +0900

    Separate ekmeans & kmeans-debugger

--- ekmeans/__init__.py
+++ ekmeans/__init__.py
@@ -1,9 +1,6 @@
-__version__ = '0.0.3'
-
-from . import debug
+__version__ = '0.0.4'
 
 from .cluster import EKMeans
 from .cluster import ekmeans
 from .cluster import kmeans
 from .keywords import proportion_keywords
-#from .logger import Logger
